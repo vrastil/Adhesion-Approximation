dd7b005a66976eed774faa44246000c019e92de0
Michal Vrastil <michalvrastil@seznam.cz>
2016-12-12 12:08:33 +0100

    first commit

--- include/cmd_line.h
+++
@@ -1,7 +0,0 @@
-/*
-Header for the cmd_line.cpp file
-*/
-#include "mesh.h"
-
-int handle_cmd_line(int, char**);
-int handle_cmd_line(int ac, char* av[], c_Sim_Param* sim);
--- include/mod_frozen_pot.h
+++
@@ -1,5 +0,0 @@
-/*
-Header for the mod_frozen_pot.cpp file
-*/
-
-int mod_frozen_potential(const c_Sim_Param &sim);
