bed9c538b6c6c7e654b4b7af08629cbe5f2caa4c
vrastil <michalvrastil@seznam.cz>
2017-06-13 20:23:50 +0200

    FF, AA simplified printing

--- src/frozen_flow.cpp
+++ src/frozen_flow.cpp
@@ -57,28 +57,7 @@ int frozen_flow(const Sim_Param &sim)
 		printf("Updating positions of particles...\n");
 		upd_pos_first_order(sim, APP.db, APP.particles, APP.app_field);
 		
-		if (APP.printing())
-		{
-			/* Printing positions */
-			print_par_pos_cut_small(APP.particles, sim, out_dir_app, APP.z_suffix());
-			APP.track.update_track_par(APP.particles);
-			print_track_par(APP.track, sim, out_dir_app, APP.z_suffix());
-
-			/* Printing density */
-			get_rho_from_par(APP.particles, &APP.power_aux, sim);
-			gen_dens_binned(APP.power_aux, APP.dens_binned, sim);
-			print_rho_map(APP.power_aux, sim, out_dir_app, APP.z_suffix());
-			print_dens_bin(APP.dens_binned, sim.mesh_num, out_dir_app, APP.z_suffix());
-			
-			/* Printing power spectrum */
-			fftw_execute_dft_r2c(APP.p_F, APP.power_aux);
-			pwr_spec_k(sim, APP.power_aux, &APP.power_aux);
-			gen_pow_spec_binned(sim, APP.power_aux, &APP.pwr_spec_binned);
-			print_pow_spec(APP.pwr_spec_binned, out_dir_app, APP.z_suffix());
-			print_pow_spec_diff(APP.pwr_spec_binned, APP.pwr_spec_binned_0, APP.b, out_dir_app, APP.z_suffix());
-			
-			APP.upd_supp();
-		}
+		if (APP.printing()) APP.print(sim, out_dir_app);
 		APP.upd_time();
 	}
 	print_suppression(APP.supp, sim, out_dir_app);
