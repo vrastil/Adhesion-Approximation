fix m_sq factor of 4 in python

simpy/power.py
@@ -68,19 +68,18 @@ def chi_bulk_a_n(a, chi_opt, MPL=1, CHI_A_UNITS=True):
     n = chi_opt["n"]
     return chi_bulk_a(a, chi_opt, MPL=MPL, CHI_A_UNITS=CHI_A_UNITS)/(1-n)
 
-def chi_mass_sq(a, k, cosmo, chi_opt, x_0=1, MPL=1, c_kms=299792.458):
+def chi_mass_sq(a, k, cosmo, chi_opt, MPL=1, c_kms=299792.458):
     """ return mass squared of chameleon field sitting at chi_bulk(a, 0) """
     prefactor = (3*MPL*chi_opt["beta"]*cosmo.Omega_m *pow(cosmo.H0 # beta*rho_m,0 / Mpl
-               / (cosmo.h * c_kms) # units factor for 'c = 1' and [L] = Mpc / h
-               * x_0 # dimension factor for laplacian
+               * cosmo.h / c_kms # units factor for 'c = 1' and [L] = Mpc / h
                ,2))
     # evolve rho_m,0 -> rho_m
-    prefactor /= 4*pow(a, 3)
+    prefactor /= pow(a, 3)
     return prefactor/chi_bulk_a_n(a, chi_opt, MPL=MPL, CHI_A_UNITS=False)
 
-def chi_lin_pow_spec(a, k, cosmo, chi_opt, x_0=1, MPL=1, c_kms=299792.458):
+def chi_lin_pow_spec(a, k, cosmo, chi_opt, MPL=1, c_kms=299792.458):
     """ return ndarray of linear power spectrum for chameleon in units of chi_prefactor """
-    mass_sq = chi_mass_sq(a, k, cosmo, chi_opt, x_0=x_0, MPL=MPL, c_kms=c_kms)
+    mass_sq = chi_mass_sq(a, k, cosmo, chi_opt, MPL=MPL, c_kms=c_kms)
     k = np.array(k)
     chi_mod = pow(mass_sq/(mass_sq+k*k), 2)
 
@@ -88,14 +87,6 @@ def chi_lin_pow_spec(a, k, cosmo, chi_opt, x_0=1, MPL=1, c_kms=299792.458):
         return chi_mod*np.array([fs.lin_pow_spec(a, k_, cosmo)  for k_ in k])
     else:
         return chi_mod*fs.lin_pow_spec(a, np.asscalar(k), cosmo)
-
-def chi_thin_shell_supp(a, k, cosmo, chi_opt):
-    """ return thin-shell suppresion factor """
-    mass_sq = chi_mass_sq(a, k, cosmo, chi_opt)
-    C = 2e-1
-    supp = C + (1-C)*mass_sq/(4*k**2+mass_sq)
-    
-    return supp**2
 
 def chi_trans_to_supp(a, k, Pk, cosmo, chi_opt, CHI_A_UNITS=True):
     """ transform input chameleon power spectrum to suppression according to linear prediction """